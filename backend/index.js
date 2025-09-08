b58a5a05c3dca50f8fb341c4193a8946d3ee1cfb
afreenkhan0227 <148878371+afreenkhan0227@users.noreply.github.com>
2025-09-08 22:23:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://five-onecart-frontend1-jcnn.onrender.com" , "http://localhost:5174"],
+ origin:["https://five-onecart-frontend1-jcnn.onrender.com" , "https://five-onecart-admin-m56n.onrender.com"],
  credentials:true
 }))
 
